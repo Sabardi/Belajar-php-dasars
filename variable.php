ab376a519b73008efc8ffc98ab656398ee8c1c22
Sabardi <Sabardibahari@gmail.com>
2023-09-13 07:50:47 +0800

    first comit

---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php echo "hello word <br>";
+echo " tipe data string ";
+echo "hello word <br>";
+$Nama = "sabardi";
+$Alamat = "Bagu";
+$Umur = 10;
+
+$Biodata = "Nama : $Nama,<br>  Alamat : $Alamat, <br> Umur : $Umur <br> ";
+echo $Biodata; 
+// Variable dan Tipe data end
+echo "// tipe data integer <br> ";
+$x = 20;
+$y = 10;
+$total = $x +$y;
+echo "Total x dan y adalah =  $total <br>";
+echo "// tipe data float <br>";
+$a = 20.36;
+$b = 10.76;
+$totals = $a + $b ;
+echo $totals; 
+
+?>
