6561867c4db3bb6e9770c796eef7bc1fbabb27d4
Sabardi <Sabardibahari@gmail.com>
2023-09-15 16:38:24 +0800

    loop array

--- Array.php
+++ Array.php
@@ -1,11 +1,21 @@
 <?php 
-$data = 
-//sata 1 dimensi / numrik
-    [1,2,3];
+/** Array adalah variabel khusus yang dapat menampung lebih dari satu nilai dalam satu waktu.
+ * 
+ */
 
+ $Ikan = ["Cupang","Arwana","Koi"];
+$length = count($Ikan);
+
+for ($i=0; $i < $length ; $i++) {
+    # code...
+    echo $Ikan[$i];
+    echo "<br>";
+}
+
+    //sata 1 dimensi / numrik
+$data = [1,2,3];
     echo $data[1];
-
-    $data2 = [
+$data2 = [
         [1,2,3],[14,5,6],[6,7,8]
     ];
     echo "<br>";
@@ -13,4 +23,10 @@ $data =
 
     echo "<br>";
     
+    $Buah = ["Apel","Jeruk","Mangga"];
+    echo count($Buah);
+    echo "<br>";
+    echo $Buah[0];
+    echo $Buah[1];
+    echo $Buah[2];
     ?>
---
+++ function.php
@@ -0,0 +1,16 @@
+<?php 
+/*intro function
+function NamaFungsi(){
+    echo "perintah nya"; 
+}
+NamaFungsi();
+*/
+
+//fungsi farameter start
+function Name($a, $b, $c) { #Returntype 
+    echo "a = $a, <br>  b = $b,<br>  c = $c, <br>";
+}
+Name("a","b","c"); #jumlah parameter nya 3 dan argument nya harus 3 juga
+Name("d","e","f");
+
+?>
